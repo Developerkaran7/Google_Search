203ee0313ced4a2f2766ff34300d66df45fa8ca2
karan git hub <develperkaran7@gmail.com>
2025-05-10 13:49:39 +0530

    done

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var searchBox = document.getElementById("searchBox");
+var googleicon = document.getElementById("googleicon");
+
+googleicon.onclick = function () {
+
+    searchBox.classList.toggle("active");
+
+}
